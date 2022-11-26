63de2f91dc4a8f6a5bc41498bd42dd02c3f0bb02
a-humanthing <arunkrishnakt2002@gmail.com>
2022-11-26 14:53:37 +0530

    completed olx

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
-import React, { useContext, useEffect } from 'react';
-import { BrowserRouter as Router,Route} from 'react-router-dom';
-import Signup from './Pages/Signup'
-import Login from './Pages/Login'
-import './App.css';
+import React, { useContext, useEffect } from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Signup from "./Pages/Signup"
+import Login from "./Pages/Login"
+import Create from "./Pages/Create"
+import View from "./Pages/ViewPost"
+import "./App.css"
+import Post from "./Store/PostContext"
 
 /**
  * ?  =====Import Components=====
  */
-import Home from './Pages/Home';
-import { AuthContext, FirebaseContext } from './Store/FirebaseContext';
+import Home from "./Pages/Home"
+import { AuthContext, FirebaseContext } from "./Store/FirebaseContext"
 
 function App() {
-  const {setUser} = useContext(AuthContext)
-  const {firebase} = useContext(FirebaseContext)
+  const { setUser } = useContext(AuthContext)
+  const { firebase } = useContext(FirebaseContext)
   useEffect(() => {
-    firebase.auth().onAuthStateChanged((user)=>{
+    firebase.auth().onAuthStateChanged((user) => {
       setUser(user)
     })
   })
-  
+
   return (
     <div>
-      <Router>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/signup'>
-          <Signup />
-        </Route>
-        <Route path='/login'>
-          <Login />
-        </Route>
-      </Router>
+      <Post>
+        <Router>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/view">
+            <View />
+          </Route>
+        </Router>
+      </Post>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
